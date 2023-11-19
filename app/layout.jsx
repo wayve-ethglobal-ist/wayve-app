8a2cece8c075cb2fe4cf6afea5d977d66a36886c
amrlftr <amirulfitri143@gmail.com>
2023-11-19 12:44:32 +0800

    feat: merge contracts

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,16 +3,32 @@ import React from 'react';
 import './globals.css';
 import { ThemeProvider } from 'styled-components';
 import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin';
+import {
+  ThirdwebProvider,
+  smartWallet,
+  localWallet,
+  metamaskWallet,
+} from "@thirdweb-dev/react";
+export const smartWalletConfig = smartWallet(localWallet(), {
+  factoryAddress: "0x07A2C46436F6fc96848E9ee32DAdA491B5CE2aEd",
+  gasless: true,
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ThemeProvider theme={lightTheme}>
-        <ThorinGlobalStyles />
-        <body className={`max-w-xl mx-auto px-4 bg-white`}>
-          {children}
-        </body>
-      </ThemeProvider>
+      <ThirdwebProvider
+        clientId="e072fbaa1dd5c09a9b8d147784576cbf"
+        activeChain="base-goerli"
+        supportedWallets={[smartWalletConfig]}
+      >
+        <ThemeProvider theme={lightTheme}>
+          <ThorinGlobalStyles />
+          <body className={`max-w-xl mx-auto px-4 bg-white h-screen`}>
+            {children}
+          </body>
+        </ThemeProvider>
+      </ThirdwebProvider>
     </html>
   );
 }
