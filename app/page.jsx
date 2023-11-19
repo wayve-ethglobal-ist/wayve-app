15603a61f42abaf1749a42b083d30a75a5e71bc0
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-11-19 08:08:52 +0300

    feat: worldcoin auth

--- app/page.jsx
+++ app/page.jsx
@@ -8,12 +8,64 @@ import {
   useContract,
   paymasterAPI,
 } from "@thirdweb-dev/react";
+import { IDKitWidget, ISuccessResult, CredentialType } from "@worldcoin/idkit";
 
 export default function Home() {
   const address = useAddress();
   const contractAddress = "0x9c376ecff687e22adde574330278ef626181567a";
   const { contract } = useContract(contractAddress);
   const { mutateAsync, isLoading, error } = useContractWrite(contract, "vote");
+
+  const merhabaFromWayve = () => {
+    console.log("call wallet here");
+  };
+
+  const onSuccess = (result) => {
+    // This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
+    window.alert(
+      "Successfully verified with World ID! Your nullifier hash is: " +
+        result.nullifier_hash
+    );
+  };
+
+  const handleProof = async (result) => {
+    console.log("Proof received from IDKit:\n", JSON.stringify(result)); // Log the proof from IDKit to the console for visibility
+
+    const reqBody = {
+      merkle_root: result.merkle_root,
+      nullifier_hash: result.nullifier_hash,
+      proof: result.proof,
+      credential_type: result.credential_type,
+      action: "yourActionHere", // Update this as needed
+      signal: "",
+    };
+
+    console.log(
+      "Sending proof to backend for verification:\n",
+      JSON.stringify(reqBody)
+    ); // Log the proof being sent to our backend for visibility
+
+    try {
+      const response = await fetch("http://localhost:3001/api/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(reqBody),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log("Successful response from backend:\n", data); // Log the response from our backend for visibility
+      } else {
+        throw new Error(`Error: ${response.status} - ${data.detail}`);
+      }
+    } catch (error) {
+      console.error("Verification failed:", error.message);
+    }
+  };
 
   return (
     <div className="container">
@@ -40,20 +92,18 @@ export default function Home() {
             )}
           </div>
           <div className="card">
-            {/* {address ? (
-              // <Web3Button
-              //   contractAddress=
-              //   action={(contract) =>
-              //     contract.erc1155.claim(editionDropTokenId, 1)
-              //   }
-              //   onSuccess={() => alert("Claim successful!")}
-              //   style={{ width: "100%", marginTop: "10px" }}
-              // >
-              //   Claim!
-              // </Web3Button>
-            ) : (
-              <p>Login to claim!</p>
-            )} */}
+            <IDKitWidget
+              app_id="app_e36d34413f30d6d9f59b0cd36474fda3"
+              action="merhabaFromWayve"
+              onSuccess={onSuccess} // optional callback when the proof is received
+              handleVerify={handleProof} // optional callback to handle the proof
+              credential_types={[CredentialType.Orb, CredentialType.Phone]} // optional, defaults to all credential types
+              enableTelemetry // optional, defaults to false
+            >
+              {({ open }) => (
+                <button onClick={open}>Verify with World ID</button>
+              )}
+            </IDKitWidget>
           </div>
         </div>
       </main>
